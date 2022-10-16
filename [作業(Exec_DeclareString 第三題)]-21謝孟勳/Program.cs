156f227414cdf2026c983f6c0b22cead33c02e79
Shiun <Shiun@DESKTOP-KPBEE4S>
2022-10-16 18:14:07 +0800

    加入專案檔案。

---
+++ [作業(Exec_DeclareString 第三題)]-21謝孟勳/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _作業_Exec_DeclareString_第三題___21謝孟勳
+{
+    //我是第一行
+    //我是第二行
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("我是第一行");
+            Console.WriteLine("我是第二行");
+        }
+    }
+}
